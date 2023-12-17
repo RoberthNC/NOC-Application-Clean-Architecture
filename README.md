229a9c8836a626379aee638699397e040d0c2926
Roberto Núñez <robertonc1599@gmail.com>
2023-12-16 22:20:37 -0500

    DotEnv and Env-Var have been installed

---
+++ README.md
@@ -0,0 +1,14 @@
+# NOC Project
+
+The goal of this project is create a sequence of tasks using Clean Architecture with Typescript
+
+# dev
+1. Clone the .env.template and rename it as .env, then set up your values for each env variable
+2. Configure env variables
+
+```
+PORT = 5000
+MAILER_EMAIL = 
+MAILER_SECRET_KEY = 
+PROD = false
+```
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
+import { envs } from "./config/plugins/envs.plugin";
 import { Server } from "./presentation/server";
+
 
 (async () => {
   main();
 })();
 
 function main() {
-  Server.start();
+  // Server.start();
+  // console.log({port: envs.PORT});
+  // console.log({port: envs.MAILER_EMAIL});
+  // console.log({port: envs.MAILER_SECRET_KEY});
+  // console.log({port: envs.PROD});
 }
